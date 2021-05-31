ea127626ac8048d49edece8ab6a6fb223051b9bc
sunny <qi.cao@cloudminds.com>
2021-05-31 16:40:08 +0800

    修改了错误机制

---
+++ examples/src/main.rs
@@ -0,0 +1,8 @@
+use vulcan_salute::*;
+use vulcan_salute::util::*;
+use vulcan_salute::util::cpu::*;
+fn main() {
+    println!("Hello, world!{:#?}",get_os_info());
+    println!("Hello, world!{:#?}",read_issue().unwrap().trim());
+    println!("cpu:{}",get_cpu());
+}
