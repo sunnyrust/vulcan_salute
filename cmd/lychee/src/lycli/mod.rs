48ee351f4b8cd98ed2c3aa0918586963f98df447
sunny <qi.cao@cloudminds.com>
2021-06-11 17:19:40 +0800

    lychee

--- cmd/lychee/src/lycli/mod.rs
+++ cmd/lychee/src/lycli/mod.rs
@@ -1,12 +1,71 @@
 use std::fs;
-
-pub fn mkdir(path:&str){
+use serde::{Deserialize, Serialize};
+use std::fs::{ OpenOptions};
+use std::io::{Write};
+#[derive(Serialize, Deserialize)]
+#[derive(Debug)]
+pub struct Cargo { 
+     pub name : String,
+     pub version :String,
+     pub authors : String,
+     pub edition : String,
+}
+/// 创建目录
+pub(crate)  fn mkdir(path:&str){
     let r = fs::create_dir_all(path);
     match r {
         Err(e) => {
             println!("error creating {}: {}", path, e);
             std::process::exit(1);
         }
-        Ok(_) => println!("created {}: OK", path),
+        Ok(_) => println!("Project folder created {}: 👌", path),
     }
+}
+
+pub(crate) fn create_main(path:String)-> std::io::Result<()>{
+    let mut buffer = OpenOptions::new()
+    .read(true)
+    .write(true)
+    .create(true)
+    .open(&path).unwrap();
+    let str_main=r#"
+fn main(){
+    println!("Hello vulcan salute!");
+    println!("Live long and prosper!\nPeace and long life");
+}
+"#;
+    buffer.write(str_main.as_bytes())?;
+    Ok(())
+}
+
+/// 创建Cargo.toml文件
+pub(crate) fn create_caego_toml(cargo: crate::lycli::Cargo,path:String)-> std::io::Result<()>{
+    //println!("{}---{:#?}",path,cargo);
+    let mut buffer = OpenOptions::new()
+    .read(true)
+    .write(true)
+    .create(true)
+    .open(&path).unwrap();
+
+    buffer.write(b"[package]")?;
+    buffer.write(b"\n")?;
+    buffer.write(cargo.name.trim().as_bytes())?;
+    buffer.write(b"\n")?;
+    buffer.write(cargo.version.trim().as_bytes())?;
+    buffer.write(b"\n")?;
+
+    buffer.write(cargo.authors.trim().as_bytes())?;
+    buffer.write(b"\n")?;
+    buffer.write(cargo.edition.trim().as_bytes())?;
+    buffer.write(b"\n\n")?;
+
+    buffer.write(r"# See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html".as_bytes())?;
+    buffer.write(b"\n\n")?;
+    buffer.write(b"[dependencies]")?;
+    buffer.write(b"\n")?;
+
+    println!("Cargo.toml created.👌");
+    buffer.flush()?;
+
+    Ok(())
 }
--- cmd/lychee/src/main.rs
+++ cmd/lychee/src/main.rs
@@ -1,21 +1,58 @@
 //!
 //!  这个是脚手架
 //!
+
+#[macro_use]
+extern crate clap;
+extern crate libc;
+use clap::{App, SubCommand};
 extern crate git2;
 use git2::{Config};
-
+use std::{thread, time};
 mod lycli;
-use serde::{Deserialize, Serialize};
-#[derive(Serialize, Deserialize)]
-#[derive(Debug)]
-pub struct Cargo { 
-     name : String,
-     version :String,
-     authors : String,
-     edition : String,
-}
+/// flag用于参数
+fn flag() -> () {
+    let s="Version:".to_owned()+&crate_version!().to_owned()+"  git:"+&crate_description!().to_owned();
+    // println!("{:?}",s);
+    let matches = App::new("Sunnycat")
+        .version(&*s)
+        .author(crate_authors!())
+        .about(
+            "日志检索，增强版cat.
+        例子:sunnycat --keyword example --file log.txt
+                 ./sunnycat lines -r 5,10
+        ",
+        )
+        .args_from_usage("-k, --keyword=[KEYWORD] '搜索关键字'")
+        .args_from_usage("-b, --bytekeyword=[BYTEKEYWORD] '搜索byte关键字'")
+        .args_from_usage("-s,--str=[STRING]'转成中文'")
+        .args_from_usage("-f ,--file=[FILE] 'Sets the input file to use'")
+        .subcommand(
+            App::new("lineonly")
+                .about("只显示有关键字存在的行号")
+                .version(crate_version!())
+                .args_from_usage("-l, --list '只显示能够查找到关键字的行号。'" ,)
+        )
+        .subcommand(
+            SubCommand::with_name("lines")
+                .about("选择哪些行显示")
+                .version(crate_version!())
+                .author("Sunny Region. <jinheking@gmail.com>")
+                .args_from_usage(
+                    "-r --rows '输入行数，例如：-r 1,10,表示从第一行到第10行。'
+                                    [LINES]  ' 1,10,表示从第一行到第10行。'",
+                ),
+        )
+        .get_matches();
+    }
 
 fn main() {
+    unsafe {
+        libc::signal(libc::SIGPIPE, libc::SIG_DFL);
+    }
+    flag();
+
+
     let cfg = Config::open_default().unwrap();
     let mut name_value:String="".to_string();
     let mut email_value:String="".to_string();
@@ -30,10 +67,20 @@ fn main() {
         email_value=entry.value().unwrap().to_string();
        }
     }
-    let mut authors=String::from("[")+&name_value+" <"+&email_value+">]";
+    let project_name="sunny";  //TODO 接收项目名称
+    let authors=String::from("[\"")+&name_value+" <"+&email_value+">\"]";
+    let  edition="2018";   //TODO 接收edition 版本
+    let cargo=lycli::Cargo{name:"name = \"".to_string()+&project_name+"\"",version:"version = \"0.1.0\"".to_string(),authors:"authors = ".to_string()+&authors,edition:"edition = \"".to_string()+&edition+"\""};
+    //println!("{:#?}",cargo);
 
-    let cargo=Cargo{name:"name=".to_string()+&name_value,version:"0.1.0".to_string(),authors:authors,edition:"2018".to_string()};
-    println!("{:#?}",cargo);
-
-    lycli::mkdir("./hello/src");
+    
+    let cargo_toml_path=project_name.to_string()+"/Cargo.toml";
+    let dir_name=project_name.to_string()+"/src";
+    lycli::mkdir(&dir_name);
+    let millis = time::Duration::from_millis(300);
+    thread::sleep(millis);
+    let _result=lycli::create_caego_toml(cargo, cargo_toml_path);
+    thread::sleep(millis);
+    let main_path=project_name.to_string()+"/src/main.rs";
+    let _result=lycli::create_main(main_path);
 }
